56ef23a86d7b2dd5371ce02b7ce70b5a56ef34aa
karichalobato <00002517@uca.edu.sv>
2019-03-29 13:48:28 -0600

    Manda datos de usuario y correo

--- app/src/main/java/com/saenz/evaluacion01_pdm_00002517/MainActivity.java
+++ app/src/main/java/com/saenz/evaluacion01_pdm_00002517/MainActivity.java
@@ -1,13 +1,38 @@
 package com.saenz.evaluacion01_pdm_00002517;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bt_enviar;
+    private EditText mUsuario, mCorreo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUsuario = findViewById(R.id.Edit_text1);
+        mCorreo = findViewById(R.id.Edit_text2);
+        bt_enviar = findViewById(R.id.btn_enviar);
+        bt_enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuario = mUsuario.getText().toString();
+                String correo = mCorreo.getText().toString();
+                Intent mIntent = new Intent(MainActivity.this, NewActivity.class);
+                mIntent.putExtra("usuario", usuario);
+                mIntent.putExtra("correo", correo);
+                MainActivity.this.startActivity(mIntent);
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/saenz/evaluacion01_pdm_00002517/NewActivity.java
+++ app/src/main/java/com/saenz/evaluacion01_pdm_00002517/NewActivity.java
@@ -1,4 +1,27 @@
 package com.saenz.evaluacion01_pdm_00002517;
 
-public class NewActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class NewActivity extends AppCompatActivity {
+
+    private TextView nUsuario, nCorreo;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+        nUsuario = findViewById(R.id.nTV_01);
+        nCorreo = findViewById(R.id.nTV_02);
+        Intent mIntent = getIntent();
+        if(mIntent!= null){
+            nUsuario.setText("Usuario: " + mIntent.getStringExtra("usuario"));
+            nCorreo.setText("Correo: " + mIntent.getStringExtra("correo"));
+        }
+
+    }
 }
